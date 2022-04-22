fcee18e3599d79041b9e4c0ac5ff8be851c04a31
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-22 16:21:54 +1000

    added images to the project

---
+++ src/components/menuBar.jsx
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+    return(
+        <>
+            <button>Snap</button>
+            <button>Featuring</button>
+            <button>Company</button>
+            <button>Careers</button>
+            <button>About</button>
+            <button>Login</button>
+            <button>Register</button>
+        </>
+    )
+}
+
+export default NavBar
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,8 +1,17 @@
+import NavBar from "../components/menuBar"
+// import { ReactComponent as ImageBrowser} from  
 
 const HomePage = () => {
     return (
-        <>
-            <h1>Home Page</h1>
+        <>  
+            <NavBar />
+            <h1>Make remote work home</h1>
+            <p>
+                Get your team in sync, no matter your location.
+                Streamline processes, create tean rituals, and watch productivity.
+            </p>
+
+            <button>Learn More</button>
         </>
     )
 }
