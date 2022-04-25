1cc65ebadec675e00c41d44edd86b4a384a4e451
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-25 11:04:12 +1000

    styling of sign up buttons

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,43 +1,49 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-import Logo from "../imagesSvg/logo.svg"
+import Logo from "../imagesSvg/logo.svg";
 
 const NavBarDiv = styled.div`
-    height: 50px;
-    border: solid red 1px;
-`
+  height: 50px;
+  border: solid red 1px;
+`;
 
 const SnapLogo = styled.img`
-    margin: auto 20px;
-    position: relative;
-    top: 15px;
-`
+  margin: auto 20px;
+  position: relative;
+  top: 14px;
+`;
 
 const Button = styled.button`
-    font-weight: bold;
-    border: none;
-    margin: 0 10px;
-    background-color: hsl(0, 0%, 98%);
-`
-
-const ButtonRight =styled.button`
-
-`
-
-
+  border: none;
+  margin: 0 10px;
+  background-color: hsl(0, 0%, 98%);
+`;
+const ButtonRight = styled.button`
+    width: 70px;
+    margin-right: 20px;
+  border: 1px solid hsl(0, 0%, 98%);
+  padding: 6px 0px;
+  background-color: hsl(0, 0%, 98%);
+  position: relative;
+  left: 760px;
+  :hover {
+    border: black 1px solid;
+    border-radius: 8px;
+  }
+`;
 
 const NavBar = () => {
-    return(
-        <NavBarDiv>
-            <SnapLogo src={Logo} alt='Logo' />
-            <Button>Featuring</Button>
-            <Button>Company</Button>
-            <Button>Careers</Button>
-            <Button>About</Button>
-            <ButtonRight>Login</ButtonRight>
-            <ButtonRight>Register</ButtonRight>
-        </NavBarDiv>
-    )
-}
+  return (
+    <NavBarDiv>
+      <SnapLogo src={Logo} alt="Logo" />
+      <Button>Featuring</Button>
+      <Button>Company</Button>
+      <Button>Careers</Button>
+      <Button>About</Button>
+      <ButtonRight>Login</ButtonRight>
+      <ButtonRight>Register</ButtonRight>
+    </NavBarDiv>
+  );
+};
 
-export default NavBar
+export default NavBar;
